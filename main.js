cc772271cf2b85a728b849acb051dc90ff786bbc
Paulina Ceciliano <vce06799@innovaccion.mx>
2022-03-06 12:25:28 -0600

    add form

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const elementosCarousel = document.querySelectorAll('.carousel');
     M.Carousel.init(elementosCarousel, {
-        duration: 50,
+        duration: 150,
         dist: -80,
         shift: 5,
         padding: 5,
